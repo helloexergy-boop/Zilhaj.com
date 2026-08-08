234653a6adb87fede6f0567dcbc7412981e320bc
Animesh <rajuranjanxbkj@gmail.com>
2026-08-08 12:24:58 +0530

    Fix crypto import in api/index.js to resolve Internal Server Error on hashPassword

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const https = require('https');
+const crypto = require('crypto');
 const nodemailer = require('nodemailer');
 const { MongoClient } = require('mongodb');
 
